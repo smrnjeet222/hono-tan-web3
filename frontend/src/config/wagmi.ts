2909509af542d2afa5309d412226255290fde94d
jeet <smrnjeet.dev@gmail.com>
2024-12-06 04:07:04 +0530

    fix: remove zod

--- frontend/src/config/wagmi.ts
+++ frontend/src/config/wagmi.ts
@@ -1,21 +1,20 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { http } from "viem";
 import { arbitrum, base, mainnet } from "wagmi/chains";
-import env from "./env";
 
 export const wagmiConfig = getDefaultConfig({
 	appName: "Web3 Tanstack",
-	projectId: env.VITE_WALLET_CONNECT_PROJECT_ID,
+	projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
 	chains: [mainnet, arbitrum, base],
 	transports: {
 		[mainnet.id]: http(
-			`https://eth-mainnet.g.alchemy.com/v2/${env.VITE_ALCHEMY_KEY}`,
+			`https://eth-mainnet.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_KEY}`,
 		),
 		[arbitrum.id]: http(
-			`https://arb-mainnet.g.alchemy.com/v2/${env.VITE_ALCHEMY_KEY}`,
+			`https://arb-mainnet.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_KEY}`,
 		),
 		[base.id]: http(
-			`https://base-mainnet.g.alchemy.com/v2/${env.VITE_ALCHEMY_KEY}`,
+			`https://base-mainnet.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_KEY}`,
 		),
 	},
 });
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+	readonly VITE_WALLET_CONNECT_PROJECT_ID: string;
+	readonly VITE_ALCHEMY_KEY: string;
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
